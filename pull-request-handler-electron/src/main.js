56c44512e3406f1551ab203f40fb72c18bc3cae2
BenCohen <benehmad@gmail.com>
2023-09-21 01:30:13 +0300

    init

--- pull-request-handler-electron/src/main.js
+++ pull-request-handler-electron/src/main.js
@@ -1,20 +1,112 @@
-const { app, Tray, Menu, nativeImage} = require('electron');
-const {join} = require("path");
-const {getIcon} = require("./utils");
-let mainWindow = null;
-let tray = null;
-app.whenReady().then(() => {
+const { app, Tray, Menu, nativeImage, BrowserWindow } = require('electron');
+const { getIcon } = require("./utils");
+
+const { initializeApp } = require('@firebase/app');
+const { getFunctions, httpsCallable } = require('@firebase/functions');
+const { getFirestore, collection, getDocs } = require('@firebase/firestore');
+const { getAuth, GithubAuthProvider, signInWithPopup, signInWithCustomToken} = require('@firebase/auth');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCrOVODfIwv0St5W9aeD80k1rJRDwXqbDE",
+    authDomain: "pull-requests-handler.firebaseapp.com",
+    databaseURL: "https://pull-requests-handler-default-rtdb.firebaseio.com",
+    projectId: "pull-requests-handler",
+    storageBucket: "pull-requests-handler.appspot.com",
+    messagingSenderId: "207968340936",
+    appId: "1:207968340936:web:a79ed6867bb8f0996f0efd"
+};
+
+const appInstance = initializeApp(firebaseConfig);
+const functionsInstance = getFunctions(appInstance);
+const authInstance = getAuth(appInstance);
+const db = getFirestore(appInstance);
+
+// Define the reference to your collection
+const dataRef = collection(db, 'tests'); // Replace with your collection's name
+
+// GitHub Authentication
+const githubOauthUrl = `https://github.com/login/oauth/authorize?client_id=YOUR_GITHUB_CLIENT_ID&redirect_uri=http://localhost:8000/callback&scope=user&state=SOME_RANDOM_STRING`;
+
+// GitHub Authentication using BrowserWindow
+const signInWithGithub = () => {
+    return new Promise((resolve, reject) => {
+        let authWindow = new BrowserWindow({
+            width: 500,
+            height: 600,
+            show: true,
+            modal: true,
+            webPreferences: {
+                nodeIntegration: false
+            }
+        });
+
+        authWindow.loadURL(githubOauthUrl);
+        authWindow.on('closed', () => {
+            reject(new Error('Window was closed by user'));
+        });
+
+        authWindow.webContents.on('will-redirect', (event, url) => {
+            if (url.startsWith('http://localhost:8000/callback')) {
+                const matched = url.match(/code=([\w_\-]+)/i);
+                if (matched && matched[1]) {
+                    resolve(matched[1]);
+                    authWindow.close();
+                } else {
+                    reject(new Error('No code found in GitHub OAuth response'));
+                }
+            }
+        });
+    });
+};
+
+// Fetch Data from Firestore and Build Menu
+const fetchDataAndBuildMenu = async (tray) => {
+    let menuItems = [];
+
+    try {
+        const snapshot = await getDocs(dataRef);
+        snapshot.forEach(doc => {
+            menuItems.push({
+                label: doc.data().YOUR_FIELD_NAME, // Replace with your field's name
+                type: 'radio'
+            });
+        });
+
+        const contextMenu = Menu.buildFromTemplate(menuItems);
+        tray.setContextMenu(contextMenu);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+};
+
+const getFirebaseTokenFromGithubCode = async (code) => {
+    const getTokenFunction = httpsCallable(functionsInstance, 'exchangeGithubCodeForFirebaseToken');
+    try {
+        const result = await getTokenFunction({ code });
+        return result.data.token;
+    } catch (error) {
+        console.error('Error fetching Firebase token:', error);
+        throw error;
+    }
+};
+
+app.whenReady().then(async () => {
     const iconBase64 = getIcon();
-    const icon = nativeImage.createFromDataURL(iconBase64)
-    tray = new Tray(icon)
+    const icon = nativeImage.createFromDataURL(iconBase64);
+    const tray = new Tray(icon);
 
-    const contextMenu = Menu.buildFromTemplate([
-        { label: 'Item1', type: 'radio' },
-        { label: 'Item2', type: 'radio' },
-        { label: 'Item3', type: 'radio', checked: true },
-        { label: 'Item4', type: 'radio' }
-    ])
+    try {
+        const code = await signInWithGithub();
 
-    tray.setToolTip('This is my application.')
-    tray.setContextMenu(contextMenu)
-})
+        // Use the Firebase function to get Firebase custom token
+        const firebaseToken = await getFirebaseTokenFromGithubCode(code);
+
+        // Now sign in to Firebase with the custom token
+        await signInWithCustomToken(authInstance, firebaseToken);
+
+        // ... [rest of your code]
+    } catch (error) {
+        console.error('Authentication error:', error);
+    }
+});
+
--- pull-requests-handler-firebase/functions/index.js
+++ pull-requests-handler-firebase/functions/index.js
@@ -1,4 +1,6 @@
 const { getDatabase, ref, set, remove, update } = require("firebase/database");
+const { oauth2 } = require('google-auth-library');
+const admin = require('firebase-admin');
 
 const functions = require("firebase-functions");
 const { initializeApp } = require("firebase/app");
@@ -20,7 +22,6 @@ const app = initializeApp(firebaseConfig);
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest(async (request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
   console.log('request', request.body);
   const { review: { state: reviewState } = {}, pull_request: { id: pullRequestId, user, state, html_url: url, title }, action, mergeable, mergeable_state, requested_reviewer } = request.body
   const { id: userId, login: username } = user
@@ -77,4 +78,32 @@ exports.helloWorld = functions.https.onRequest(async (request, response) => {
 
   console.log('end');
   response.send(`event" ${request.body}`);
+});
+
+exports.exchangeGithubCodeForFirebaseToken = functions.https.onCall(async (data, context) => {
+  const CLIENT_ID = 'Iv1.bb0106805da87921'; // Your GitHub Client ID
+  const CLIENT_SECRET = '12f4cb2707c54e083f4707613bfecbd9db40eeb6'; // Your GitHub Client Secret
+
+  const { code } = data;
+
+  const client = new oauth2.OAuth2Client(CLIENT_ID, CLIENT_SECRET, 'http://localhost:8000/callback'); // The callback URL
+
+  // Exchange the code for an access token
+  const { tokens } = await client.getToken(code);
+
+  // Now let's get the user's GitHub username using the access token
+  const octokit = new Octokit({ auth: tokens.access_token });
+  const user = await octokit.rest.users.getAuthenticated();
+
+  // Here, we're using the GitHub username as the uid in Firebase. You can adjust this if needed.
+  const firebaseToken = await admin.auth().createCustomToken(user.data.login);
+
+  return { token: firebaseToken };
+});
+
+exports.myFunction = functions.https.onCall((data, context) => {
+  console.log('start');
+  return {
+    result: 'Success'
+  };
 });
